8860ad986abb5b6034a9a72102320523b0336c14
ntrupin <natrupin@gmail.com>
2023-07-19 22:46:21 -0400

    deprecated alpine info

--- deprecated/alpine/config.sh
+++ deprecated/alpine/config.sh
@@ -1,5 +1,7 @@
 #!/bin/ash
 
+# Created June 21, 2020
+
 base() {
     wget -qO- http://dl-cdn.alpinelinux.org/alpine/v3.12/main/x86/apk-tools-static-2.10.5-r1.apk | tar -xz sbin/apk.static && ./sbin/apk.static add apk-tools && rm sbin/apk.static && rmdir sbin 2> /dev/null
 }
